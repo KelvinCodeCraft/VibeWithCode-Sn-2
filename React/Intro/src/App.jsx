caf1267bc7e53fb0e8bce505ed8a23cb30cc9633
Kelvin Kipchumba <koechkelvin2001@gmail.com>
2025-07-02 11:31:51 +0300

    props

--- React/Intro/src/App.jsx
+++ React/Intro/src/App.jsx
@@ -1,19 +1,60 @@
 import React from "react";
-import './App.css';
-import About from "./Components/About/about";
-import Form from "./Components/Form/form";
+
+function Greetings({ name }) {
+  return <h3>Hello, {name} welcome to our app</h3>;
+}
+
+function UserList({ users }) {
+  return (
+    <ol>
+      {users.map((user, index) => (
+        <li key={index}>
+          {user}
+        </li>
+      ))}
+    </ol>
+  )
+}
+
+function ClickButton({ onClick }) {
+  return <button onClick={onClick}>Click Me!</button>;
+}
+
+function Color({ color }) {
+  const boxStyle = {
+    width: "100px",
+    height: "100px",
+    backgroundColor: color,
+    border: "1px solid black",
+  };
+  return <div style={boxStyle}></div>;
+}
+
+function Status({ isOnline }) {
+  return (
+    <div>
+      {isOnline ? <p>User is online</p> : <p>User is offline</p>}
+    </div>
+  );
+}
 
 function App() {
-  const name = "React";
-  const messages = [1, 2, 3];
-
+  const myUsers = ["Alice", "Bob", "Charlie"];
   return (
-    <>
-    <h1>Hello, {name}!</h1>
-    <h1>You have {messages.length} new messages.</h1>
-    </>
-    
-
+    <div>
+      <Greetings name="John" />
+      <Greetings name="Jane" />
+      <Greetings name="Doe" />
+      <h2>Users:</h2>
+      <UserList users={myUsers} />
+      <ClickButton onClick={() => console.log("Button clicked!")} />
+      <>
+      <Color color="blue" />
+      <Color color="red" />
+      </>
+      <Status isOnline={true} />
+      <Status isOnline={false} />
+    </div>
   );
 }
 
